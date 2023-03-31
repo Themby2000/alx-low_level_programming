da65b775ad0c1bdb353ba89013fdc781a072fb39
Themby2000 <sthembie@gmail.com>
2023-03-31 13:26:55 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -3,10 +3,11 @@
 
 /**
  *main - check the code
+ *print_array
  *@a: an array of integers
  *@n: the number of elements to swap
  *
- *Return: nothing.
+ * Return: nothing.
  */
 void print_array(int *a, int n)
 {
@@ -16,10 +17,26 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 	if (i != 0)
+	{
 	printf(", ");
 	}
 	printf("%d", a[i]);
 	i++;
 	}
 	printf("\n");
+}
+
+/**
+ * main - check the code
+ * print_array
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	return (0);
 }
