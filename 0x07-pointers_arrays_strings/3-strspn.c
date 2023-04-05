cd869f6b56b4cffefea84e268d92d42d7f77e839
Themby2000 <sthembie@gmail.com>
2023-04-04 19:20:00 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - compute length matching contigous bytes
+ * @s: string to calculate substring length from
+ * @accept: string containing bytes to match
+ * Return: length of matching contigous bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0, match = 0, i;
+
+	while (*s)
+{
+	for (i = 0; accept[i]; i++)
+	{
+		if (*s == accept[i])
+		{
+			count++;
+			match = 1;
+		}
+	}
+	if (!match)
+		return (count);
+	match = 0;
+	s++;
+}
+return (count);
+}
