12c0727beba5938443ad0120ab9617b3dc58d1b6
Themby2000 <sthembie@gmail.com>
2023-05-01 03:41:35 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *print_list - print elements in a linked list
+ *@h: name of the list
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int  count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
+		h = h->next;
+		}
+	return (count);
+}
